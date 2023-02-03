7b36dc5610c2b00ff666a97ffed555569f3b72f5
Qwinci <32550582+Qwinci@users.noreply.github.com>
2023-02-03 23:54:53 +0200

    Small cleanup

--- src/a.asm
+++
@@ -1,6 +0,0 @@
-global test_user_fn
-
-test_user_fn:
-	mov eax, 0
-	xor rdi, rdi
-	syscall
--- src/usermode/usermode.asm
+++ src/usermode/usermode.asm
@@ -11,12 +11,12 @@ usermode_ret:
 	pop rcx
 
 	xor rax, rax
-    xor rdx, rdx
-    xor rsi, rsi
-    xor rdi, rdi
-    xor r8, r8
-    xor r9, r9
-    xor r10, r10
+	xor rdx, rdx
+	xor rsi, rsi
+	xor rdi, rdi
+	xor r8, r8
+	xor r9, r9
+	xor r10, r10
 
 	mov r11, 0x202
 	o64 sysret
