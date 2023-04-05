ec5434d018a1c5f9d5f59ab5df78391076104454
Qwinci <32550582+Qwinci@users.noreply.github.com>
2023-04-05 23:30:35 +0300

    kmain: Remove temporary testing variables

--- src/main.c
+++ src/main.c
@@ -1,17 +1,8 @@
 #include "arch/misc.h"
-#include "mem/pmalloc.h"
 #include "stdio.h"
 
 [[noreturn]] void kmain() {
 	kprintf("[kernel]: entered main\n");
-
-	Page* first = pmalloc(2);
-	Page* second = pmalloc(2);
-	kprintf("%p\n", first);
-	kprintf("%p\n", second);
-	kprintf(first->phys + 0x2000 == second->phys ? "true\n" : "false\n");
-	pfree(first, 2);
-	pfree(second, 2);
 	while (true) {
 		arch_hlt();
 	}
