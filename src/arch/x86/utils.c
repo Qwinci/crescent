c6186e3ae080df6055d968a3785565f4c8a3493f
Qwinci <32550582+Qwinci@users.noreply.github.com>
2023-04-05 23:24:46 +0300

    x86: critical sections

--- src/arch/x86/utils.c
+++ src/arch/x86/utils.c
@@ -1,4 +1,5 @@
 #include "arch/misc.h"
+#include "types.h"
 
 void arch_hlt() {
 	__asm__ volatile("hlt");
@@ -6,4 +7,24 @@ void arch_hlt() {
 
 void arch_spinloop_hint() {
 	__builtin_ia32_pause();
+}
+
+static bool x86_disable_interrupts_with_prev() {
+	u16 flags;
+	__asm__ volatile("pushfw; pop %0; cli" : "=r"(flags));
+	return flags & 1 << 9;
+}
+
+static void x86_enable_interrupts() {
+	__asm__ volatile("sti");
+}
+
+void* enter_critical() {
+	return (void*) x86_disable_interrupts_with_prev();
+}
+
+void leave_critical(void* flags) {
+	if (flags) {
+		x86_enable_interrupts();
+	}
 }
