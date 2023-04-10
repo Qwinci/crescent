edd798a33b983417f8924e7c1ea74d560750efe0
Qwinci <32550582+Qwinci@users.noreply.github.com>
2023-04-10 16:37:16 +0300

    cmake: Move some linker arguments out of add_link_options

--- .config/toolchain.cmake
+++ .config/toolchain.cmake
@@ -3,7 +3,7 @@ set(CMAKE_SYSTEM_NAME Generic)
 set(triple x86_64-unknown-none-elf)
 set(CMAKE_C_COMPILER clang CACHE STRING "" FORCE)
 set(CMAKE_C_COMPILER_TARGET ${triple})
-add_link_options(-fuse-ld=lld -T ${PROJECT_SOURCE_DIR}/.config/kernel.ld -static -static-pie -nostdlib -Wl,--fatal-warnings)
+add_link_options(-fuse-ld=lld -static -static-pie -nostdlib -Wl,--fatal-warnings)
 
 enable_language(C ASM_NASM)
 set(CMAKE_ASM_NASM_OBJECT_FORMAT elf64)
