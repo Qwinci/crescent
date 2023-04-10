a1487bef3987fbcd965d5f075e1f148c7757cfcf
Qwinci <32550582+Qwinci@users.noreply.github.com>
2023-04-10 16:38:32 +0300

    sys: Add a prototype create_thread syscall

--- src/sys/syscalls.c
+++ src/sys/syscalls.c
@@ -1,12 +1,33 @@
-#include "types.h"
+#include "arch/cpu.h"
+#include "arch/misc.h"
 #include "sched/sched.h"
+#include "stdio.h"
+#include "types.h"
 
-usize syscall_handler_count = 1;
+usize syscall_handler_count = 2;
 
-void sys_exit() {
+void sys_exit(int status);
+Task* sys_create_thread(void (*fn)(), void* arg);
+
+void* syscall_handlers[] = {
+	sys_exit,
+	sys_create_thread
+};
+
+void sys_exit(int status) {
+	Task* task = arch_get_cur_task();
+	kprintf("task '%s' exited with status %d\n", task->name, status);
 	sched_exit();
 }
 
-void* syscall_handlers[] = {
-	sys_exit
-};
+Task* sys_create_thread(void (*fn)(), void* arg) {
+	Task* task = arch_create_user_task("", fn, arg, arch_get_cur_task());
+	if (!task) {
+		return NULL;
+	}
+	Task* self = arch_get_cur_task();
+	void* flags = enter_critical();
+	sched_queue_task(task);
+	leave_critical(flags);
+	return task;
+}
--- tests/basic.asm
+++ tests/basic.asm
@@ -1,7 +1,23 @@
-global start
+global _start
 
-start:
+%define SYS_EXIT 0
+%define SYS_CREATE_THREAD 1
+
+thread:
+	mov edi, SYS_EXIT
+	mov eax, 1
+	syscall
+_start:
 	; num arg0 arg1 arg2 arg3 arg4 arg5
 	; rdi rax  rsi  rdx  r10  r8   r9
-	mov edi, 0
+
+	; create a thread
+	mov edi, SYS_CREATE_THREAD
+	lea rax, [rel thread]
+	xor esi, esi
+	syscall
+
+	; exit
+	mov edi, SYS_EXIT
+	xor eax, eax
 	syscall
